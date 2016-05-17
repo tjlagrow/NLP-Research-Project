1743b286e35f582bbd420cb0adaca0a8e9090c43
Jacob Bieker <jacob@bieker.tech>
2016-05-17 00:03:23 -0700

    fices stuff

--- analysis.py
+++ analysis.py
@@ -71,7 +71,7 @@ def searching():
             print("Ngrams")
             print(clean)
             print(fdist.most_common(20))
-            with open("ngram-intermediate.txt", "w") as outputty:
+            with open("ngram-intermediate.txt", "a") as outputty:
                 outputty.write('\n'.join('{} {} {} {} {} {} {} {} {} {} {} {} {} {} {}'.format(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10],x[11],x[12],x[13],x[14]) for x in clean))
 
             for word in interesting_words:
