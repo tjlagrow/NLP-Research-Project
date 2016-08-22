2984d9c904349d3d8326717a8d8899ce5842626b
Jacob Bieker <jacob@bieker.tech>
2016-08-22 03:44:51 -0700

    Reorganize and start tests

--- tests/extractionTests.py
+++ tests/extractionTests.py
@@ -1,1 +1,18 @@
 __author__ = 'jacob'
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_abstract(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_citation(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_references(self):
+        self.assertEqual(True, 1)
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/meaningTest.py
+++ tests/meaningTest.py
@@ -1,1 +1,3 @@
 __author__ = 'jacob'
+import unittest
+
